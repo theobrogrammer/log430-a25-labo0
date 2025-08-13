124f25b38de40e27ccf1f56707db2485cd5de64e
Gabriel C. Ullmann <ullmanng162@gmail.com>
2025-08-12 20:58:12 -0400

    fix test

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,6 +1,7 @@
-from test_main import hello
+from main import Calculator
 
 def test_app():
-    assert hello() == "Allo Calculatrice"
+    my_calculator = Calculator()
+    assert my_calculator.hello() == "Allo Calculatrice"
 
 # TODO: ajoutez les tests
